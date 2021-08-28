7b2e54f33e60f9229ba00d9cde8c64ab3043f518
kmh339 <kmh339@gmail.com>
2021-08-28 21:32:35 +0900

    add folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app/my_home/my_home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -15,49 +17,6 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(
         title: 'GraphQL',
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final ValueNotifier<int> _counter = ValueNotifier<int>(0);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            ValueListenableBuilder<int>(
-              valueListenable: _counter,
-              builder: (BuildContext context, int value, _) => Text(
-                '$value',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _counter.value += 1,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/service/gql_service.dart
+++ lib/service/gql_service.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GqlService {
+  static final GqlService instance = GqlService();
+
   static final HttpLink httpLink = HttpLink('https://api.github.com/graphql');
 
   final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
